4e489a5bc4a15e38dcbdaa5df6e818b52bf353fc
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-28 12:44:19 +0000

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/qlhanghoa', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hệ thống quản lý hàng hóa đang hoạt động');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server đang chạy trên cổng ${PORT}`);
+});
